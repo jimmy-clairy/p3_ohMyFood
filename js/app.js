84f6afcb1588a795bf357bf0c39df29fb75208d3
Jimmy Clairy <jimcl@hotmail.fr>
2023-04-21 14:47:13 +0200

    add matchMedia min-width:800px

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
-const heart = document.querySelectorAll('.heart')
-heart.forEach(h => h.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (window.innerWidth > 1000) {
+// If media 
+if (window.matchMedia("(min-width: 800px)").matches) {
+    const heart = document.querySelectorAll('.heart')
+    heart.forEach(h => h.addEventListener('click', (e) => {
+        e.preventDefault()
         h.classList.toggle('active')
-    }
-}))
+    }))
 
-const divMenu = document.querySelectorAll('.submenu__container')
-divMenu.forEach(d => d.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (window.innerWidth > 1000) {
+    const divMenu = document.querySelectorAll('.submenu__container')
+    divMenu.forEach(d => d.addEventListener('click', (e) => {
+        e.preventDefault()
         d.classList.toggle('active')
-    }
-}))
+    }))
+
+} else {
+    const heart = document.querySelectorAll('.heart')
+    heart.forEach(h => h.addEventListener('touchstart', (e) => {
+        e.preventDefault()
+        h.classList.toggle('active')
+    }))
+
+    const divMenu = document.querySelectorAll('.submenu__container')
+    divMenu.forEach(d => d.addEventListener('touchstart', (e) => {
+        e.preventDefault()
+        d.classList.toggle('active')
+    }))
+}
