1c5865e285454c0e0bec9923204b9d105ddcf849
Jimmy Clairy <jimcl@hotmail.fr>
2023-04-21 14:28:09 +0200

    add app.js window.innerWidth > 1000

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-console.log('OhmyFood');
-
 const heart = document.querySelectorAll('.heart')
 heart.forEach(h => h.addEventListener('click', (e) => {
     e.preventDefault()
-    h.classList.toggle('active')
+    if (window.innerWidth > 1000) {
+        h.classList.toggle('active')
+    }
 }))
 
 const divMenu = document.querySelectorAll('.submenu__container')
 divMenu.forEach(d => d.addEventListener('click', (e) => {
     e.preventDefault()
-    d.classList.toggle('active')
+    if (window.innerWidth > 1000) {
+        d.classList.toggle('active')
+    }
 }))
