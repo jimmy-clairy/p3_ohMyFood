6e5df079962a3bf46cf8a4ce8eeae7bf20fcf2c4
Jimmy Clairy <jimcl@hotmail.fr>
2023-05-06 12:01:55 +0200

    Remove highlight

--- js/app.js
+++ js/app.js
@@ -1,27 +1,12 @@
-// If media 
-if (window.matchMedia("(min-width: 800px)").matches) {
-    const heart = document.querySelectorAll('.heart')
-    heart.forEach(h => h.addEventListener('click', (e) => {
-        e.preventDefault()
-        h.classList.toggle('active')
-    }))
+const hearts = document.querySelectorAll(".heart")
+hearts.forEach(heart => heart.addEventListener("click", (e) => {
+    e.preventDefault()
+    heart.classList.toggle('active')
+})
+)
 
-    const divMenu = document.querySelectorAll('.submenu__container')
-    divMenu.forEach(d => d.addEventListener('click', (e) => {
-        e.preventDefault()
-        d.classList.toggle('active')
-    }))
-
-} else {
-    const heart = document.querySelectorAll('.heart')
-    heart.forEach(h => h.addEventListener('touchstart', (e) => {
-        e.preventDefault()
-        h.classList.toggle('active')
-    }))
-
-    const divMenu = document.querySelectorAll('.submenu__container')
-    divMenu.forEach(d => d.addEventListener('touchstart', (e) => {
-        e.preventDefault()
-        d.classList.toggle('active')
-    }))
-}
+const divMenu = document.querySelectorAll('.submenu__container')
+divMenu.forEach(d => d.addEventListener('click', (e) => {
+    e.preventDefault()
+    d.classList.toggle("active")
+}))
